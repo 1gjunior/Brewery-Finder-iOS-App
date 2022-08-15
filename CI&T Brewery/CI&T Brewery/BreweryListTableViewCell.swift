7fe0c563ffde6c90e6551f197efce5e866b4ba76
Gilberto Junior <gjunior@ciandt.com>
2022-08-15 17:16:37 -0300

    feat: add brewery list custom cell

---
+++ CI&T Brewery/CI&T Brewery/BreweryListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BreweryListTableViewCell.swift
+//  CI&T Brewery
+//
+//  Created by Gilberto Junior on 15/08/22.
+//
+
+import UIKit
+
+class BreweryListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
