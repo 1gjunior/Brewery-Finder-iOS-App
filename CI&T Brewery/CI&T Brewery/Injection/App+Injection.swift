b6c350f75d599c62f2d5418f32d3e686e5d26136
Rafaela Souza <rafaelasouza@ciandt.com>
2022-08-15 13:59:49 -0300

    Implementando Resolver

---
+++ CI&T Brewery/CI&T Brewery/Injection/App+Injection.swift
@@ -0,0 +1,23 @@
+//
+//  App+Injection.swift
+//  CI&T Brewery
+//
+//  Created by Rafaela Cristina Souza dos Santos on 15/08/22.
+//
+
+import Foundation
+import Resolver
+
+extension Resolver: ResolverRegistering {
+    public static func registerAllServices() {
+        defaultScope = .graph
+        
+        register { APIManager() as APIManagerService }
+        
+        // MARK: - Repositories
+        register { BreweryRepository(apiManager: resolve()) as BreweryRepositoryProtocol }
+        
+        // MARK: - ViewModels
+//        register { BreweryViewModel(breweryRepository: resolve()) }
+    }
+}
