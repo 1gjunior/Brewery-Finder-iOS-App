f874b7807bc43824f1a9ac8569a69683f6dc6174
Ramon Queiroz dos Santos <ramons@ciandt.com>
2022-08-15 20:07:32 -0300

    US1 - Componente de busca - Layout
    Criada a tela inicial
    adicionado elementos ao assetList
    Alteração no infoplist e sceneDelegate, view principal definida em AppDelegate

--- CI&T Brewery/CI&T Brewery/Home/View/ViewControllers/HomeViewController.swift
+++ CI&T Brewery/CI&T Brewery/Home/View/ViewControllers/HomeViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
 
     init() {
         super.init(nibName: "HomeViewController", bundle: nil)
@@ -16,9 +18,46 @@ class HomeViewController: UIViewController {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupNavigationBar()
     }
 }
 
+extension HomeViewController{
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = UIColor(red: 1, green: 0.671, blue: 0, alpha: 1)
+    
+        navigationController?.navigationBar.isTranslucent = false
+//navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setupNavigationBarItems()
+    }
+    
+    private func setupNavigationBarItems() {
+        navigationItem.title = "CI&T Brewery"
+        
+        setupLeftNavigationBar()
+        setupRightNavigationBar()
+    }
+    
+    private func setupLeftNavigationBar() {
+        let logoIcon = UIButton(type: .system)
+        logoIcon.setImage(UIImage(named: "icon"), for: .normal)
+        logoIcon.tintColor = .black
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoIcon)
+    }
+    
+    private func setupRightNavigationBar() {
+        let favoriteIcon = UIButton(type: .system)
+        favoriteIcon.setImage(UIImage(named: "favorite_border")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        let starIcon = UIButton(type: .system)
+        starIcon.setImage(UIImage(named: "star_border")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: favoriteIcon), UIBarButtonItem(customView: starIcon)]
+    }
+}
--- CI&T Brewery/CI&T Brewery/SceneDelegate.swift
+++ CI&T Brewery/CI&T Brewery/SceneDelegate.swift
@@ -13,11 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene:  windowScene)
-        window?.frame = UIScreen.main.bounds
-        window?.rootViewController = HomeViewController()
-        window?.makeKeyAndVisible()
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene:  windowScene)
+//        window?.frame = UIScreen.main.bounds
+//        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+//     //   window?.rootViewController = HomeViewController()
+//        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
