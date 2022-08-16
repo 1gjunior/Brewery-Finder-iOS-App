8a7b0afebbe744a36e7d9e39b39fdcf8395b66ee
Rafaela Souza <rafaelasouza@ciandt.com>
2022-08-16 19:16:03 -0300

    Criação do layout da BreweryDetail

---
+++ CI&T Brewery/CI&T Brewery/BreweryDetail/View/ViewController/BreweryDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  BreweryDetailViewController.swift
+//  CI&T Brewery
+//
+//  Created by Rafaela Cristina Souza dos Santos on 16/08/22.
+//
+
+import Foundation
+import UIKit
+
+class BreweryDetailViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: "BreweryDetailViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+    }
+}
+
+extension BreweryDetailViewController {
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = UIColor(red: 1, green: 0.671, blue: 0, alpha: 1)
+        navigationController?.navigationBar.isTranslucent = false
+        setupNavigationBarItems()
+    }
+    private func setupNavigationBarItems() {
+        setupLeftNavigationBar()
+        setupRightNavigationBar()
+    }
+    private func setupLeftNavigationBar() {
+        let logoIcon = UIButton(type: .system)
+        logoIcon.setImage(UIImage(named: "icon_back"), for: .normal)
+        logoIcon.tintColor = .black
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoIcon)
+    }
+    private func setupRightNavigationBar() {
+        let favoriteIcon = UIButton(type: .system)
+        favoriteIcon.setImage(UIImage(named: "icon_share")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        let starIcon = UIButton(type: .system)
+        starIcon.setImage(UIImage(named: "star_border")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: favoriteIcon), UIBarButtonItem(customView: starIcon)]
+    }
+}
---
+++ CI&T Brewery/CI&T Brewery/BreweryDetail/ViewModel/BreweryDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BreweryDetailViewModel.swift
+//  CI&T Brewery
+//
+//  Created by Rafaela Cristina Souza dos Santos on 16/08/22.
+//
+
+import Foundation
