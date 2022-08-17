9c21a8257e4a88c3f2f2de8aafabdd217cb072ed
Gilberto Junior <gjunior@ciandt.com>
2022-08-17 14:56:03 -0300

    refactor: remove unused functions

--- CI&T Brewery/CI&T Brewery/Home/View/BreweryListTableViewCell.swift
+++ CI&T Brewery/CI&T Brewery/Home/View/BreweryListTableViewCell.swift
@@ -11,24 +11,13 @@ class BreweryListTableViewCell: UITableViewCell {
     @IBOutlet var profileLetter: UILabel!
     @IBOutlet var name: UILabel!
     @IBOutlet var average: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
     func configure(_ cell: BreweryListTableViewCell, for brewery: Brewery) {
         cell.contentView.layer.cornerRadius = 30
         cell.profileLetter.layer.masksToBounds = true
         cell.profileLetter.layer.cornerRadius = 22
-        cell.profileLetter.center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
-        
+        cell.profileLetter.center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+
         profileLetter.text = "\(brewery.name.first ?? "A")"
         name.text = brewery.name
         average.text = "\(brewery.average)"
