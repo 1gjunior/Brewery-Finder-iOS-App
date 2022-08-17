c3792435299881628fb29d66ce36ba4529849938
Pamella <pamellav@ciandt.com>
2022-08-17 14:56:13 -0300

    us2: internacionalização dos textos e criação de uma constante para a URL

--- CI&T Brewery/CI&T Brewery/Details/OpenMap.swift
+++ CI&T Brewery/CI&T Brewery/Details/OpenMap.swift
@@ -14,11 +14,13 @@ protocol OpenMapDirectionsProtocol {
 }
 
 class OpenMapDirections: OpenMapDirectionsProtocol{
+    
+   static let urlGoogleMaps = "https://www.google.com/maps/search/?api=1&query="
         
     static func present(in viewController: UIViewController, sourceView: UIView, latitude: Double, longitude: Double) {
         let actionSheet = UIAlertController(title: "Open Location", message: "Choose an app to open direction", preferredStyle: .actionSheet)
         actionSheet.addAction(UIAlertAction(title: "Google Maps", style: .default, handler: { _ in
-            let url = URL(string: "https://www.google.com/maps/search/?api=1&query=\(latitude),\(longitude)")
+            let url = URL(string: urlGoogleMaps + "\(latitude),\(longitude)")
             UIApplication.shared.open(url!, options: [:], completionHandler: nil)
         }))
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
