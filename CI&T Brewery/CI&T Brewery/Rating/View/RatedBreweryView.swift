38eb7403637cd99a2117e824d33b76aaa929db16
Pamella <pamellav@ciandt.com>
2022-08-18 17:10:42 -0300

    criação do xib RatedBreweryView

---
+++ CI&T Brewery/CI&T Brewery/Rating/View/RatedBreweryView.swift
@@ -0,0 +1,29 @@
+//
+//  RatedBreweryView.swift
+//  CI&T Brewery
+//
+//  Created by Pamella Victoria Soares Lima on 18/08/22.
+//
+
+import Foundation
+import UIKit
+
+public class RatedBreweryView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    public func commonInit() {
+        guard let viewFromXib = Bundle.main.loadNibNamed("RatedBreweryView", owner: self, options: nil)?[0] as? UIView else { return }
+        viewFromXib.frame = self.bounds
+        addSubview(viewFromXib)
+    }
+    
+}
