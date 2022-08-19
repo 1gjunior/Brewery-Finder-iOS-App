3a90577bbe288649453130d4609196430ee82ff5
Pedro Henrique Catanduba de Andrade <phcatanduba@ciandt.com>
2022-08-19 16:41:00 -0300

    arruma teclado

--- CI&T Brewery/CI&T Brewery/Rating/RatingViewController.swift
+++ CI&T Brewery/CI&T Brewery/Rating/RatingViewController.swift
@@ -38,6 +38,26 @@ class RatingViewController: UIViewController {
         configureCheckbox()
         changeSaveButtonColor()
         setupTextField()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height - 100
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     private func setupTextField() {
