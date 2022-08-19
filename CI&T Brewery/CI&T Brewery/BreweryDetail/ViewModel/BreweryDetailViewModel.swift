9b336ede0a1e209e5ae8b8a622924ff28306bf4e
Gilberto Junior <gjunior@ciandt.com>
2022-08-19 15:48:53 -0300

    feat: created brewery detail view model

--- CI&T Brewery/CI&T Brewery/BreweryDetail/ViewModel/BreweryDetailViewModel.swift
+++ CI&T Brewery/CI&T Brewery/BreweryDetail/ViewModel/BreweryDetailViewModel.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
+enum BreweryDetailViewModelState {
+    case initial
+    case loading
+    case success(brewery: BreweryObject)
+    case genericError
+}
+
 class BreweryDetailViewModel {
+    let repository: BreweryRepository
+    @Published private(set) var state: BreweryDetailViewModelState = .initial
     
+    init(repository: BreweryRepository = BreweryRepository()) {
+        self.repository = repository
+    }
+    
+    func fetchBreweryBy(id: String) {
+        if !id.isEmpty {
+            state = .loading
+            
+            repository.getBreweryBy(id: id) { [weak self] result in
+                switch result {
+                case .failure(_):
+                    self?.state = .genericError
+                case .success(let breweryResponse):
+                    let parsedBrewery = BreweryObject(brewery: breweryResponse)
+                    self?.state = .success(brewery: parsedBrewery)
+                }
+            }
+        }
+    }
 }
