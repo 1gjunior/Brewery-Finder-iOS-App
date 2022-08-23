8cb210e370c856a0dffb9a5744d5ad58c8276128
Ramon Queiroz dos Santos <ramons@ciandt.com>
2022-08-23 15:26:02 -0300

    add estilização para view e metodo para selecionar os botões

--- CI&T Brewery/CI&T Brewery/Home/View/BrewerySortView/BrewerySortView.swift
+++ CI&T Brewery/CI&T Brewery/Home/View/BrewerySortView/BrewerySortView.swift
@@ -10,39 +10,77 @@ import UIKit
 
 public class SortView: UIView{
     
-    viewDidLoad(){
-        
-        let checkbox = CircularCheckbox(frame: CGRect(x: 70, y: 200, width: 70, height: 70))
-        let label = UILabel(frame: CGRect(x: 150, y: 200, width: 200, height: 70))
-        label.text = "aqui"
-        view.addSubview(label)
-        view?.addSubview(checkbox)
+    var byNameButtonSelected = false
+    var byRatingButtonSelected = false
+    
+    @IBOutlet weak var titleLabel: UILabel!{
+        didSet {
+            titleLabel.font = UIFont.robotoRegular(ofSize: 16)
+            titleLabel.textColor = UIColor.breweryBlack()
+            titleLabel.text = NSLocalizedString("Ordenar por", comment: "")
+        }
+    }
+    @IBOutlet weak var sortByName: UIStackView!{
+        didSet {
+            addBottomSeparator(uiStackView: sortByName)
+        }
+    }
+    @IBOutlet weak var byNameButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!{
+        didSet {
+            nameLabel.font = UIFont.robotoRegular(ofSize: 14)
+            nameLabel.textColor = UIColor.breweryBlack()
+            nameLabel.text = NSLocalizedString("Nome (A a Z)", comment: "")
+        }
     }
     
-}
-
-final class CircularCheckbox: UIView {
-    override init(frame: CGRect){
-        super.init(frame:frame)
-        
-        layer.borderWidth = 0.5
-        layer.borderColor = UIColor.secondaryLabel.cgColor
-        layer.cornerRadius = frame.size.width / 2.0
-        backgroundColor = .systemBackground
+    @IBOutlet weak var sortByRating: UIStackView!
+    @IBOutlet weak var byRatingButton: UIButton!
+    @IBOutlet weak var ratingLabel: UILabel!{
+        didSet {
+            ratingLabel.font = UIFont.robotoRegular(ofSize: 14)
+            ratingLabel.textColor = UIColor.breweryBlack()
+            ratingLabel.text = NSLocalizedString("Nota (menor para maior)", comment: "")
+        }
+    }
+    private func addBottomSeparator(uiStackView: UIStackView) {
+        uiStackView.layer.addBorder(edge: UIRectEdge.bottom, color: UIColor.breweryGrayLight(), thickness: 1.0)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setChecked(_ isChecked: Bool){
-        if isChecked{
-            backgroundColor = .systemGreen
+    @IBAction func isByNameButtonSelected(_ sender: Any) {
+        if byNameButtonSelected == false {
+            byNameButtonSelected = true
+            customizeNameButtonSelected()
         }
         else {
-            backgroundColor = .systemBackground
+            byNameButtonSelected = false
+            customizeNameButtonNoSelected()
         }
     }
+    @IBAction func isByRatingButtonSelected(_ sender: Any) {
+        if byRatingButtonSelected == false {
+            byRatingButtonSelected = true
+            customizeRatingButtonSelected()
+        }
+        else {
+            byRatingButtonSelected = false
+            customizeRatingButtonNoSelected()
+        }
+    }
+    
+    func customizeNameButtonSelected(){
+        byNameButton.setImage(UIImage(named: "RadioSelected"), for: .normal)
+    }
+    func customizeNameButtonNoSelected(){
+        byNameButton.setImage(UIImage(named: "RadioDisabled"), for: .normal)
+    }
+    func customizeRatingButtonSelected(){
+        byRatingButton.setImage(UIImage(named: "RadioSelected"), for: .normal)
+    }
+    func customizeRatingButtonNoSelected(){
+        byRatingButton.setImage(UIImage(named: "RadioDisabled"), for: .normal)
+    }
+    
 }
 
 
