2a08d57f6b03daf9a27659acf2ad2676f438c7ed
Ramon Queiroz dos Santos <ramons@ciandt.com>
2022-09-12 16:18:24 -0300

    criando data model

--- CI&T Brewery/CI&T Brewery/AppDelegate.swift
+++ CI&T Brewery/CI&T Brewery/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -26,5 +27,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+	
+	// MARK: - Core Data stack
+
+	lazy var persistentContainer: NSPersistentContainer = {
+		 let container = NSPersistentContainer(name: "CITModel")
+		 container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+			  if let error = error as NSError? {
+				  // Replace this implementation with code to handle the error appropriately.
+				  // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+					fatalError("Unresolved error \(error), \(error.userInfo)")
+			  }
+		 })
+		 return container
+	}()
+
+	// MARK: - Core Data Saving support
+
+	func saveContext () {
+		 let context = persistentContainer.viewContext
+		 if context.hasChanges {
+			  do {
+					try context.save()
+			  } catch {
+					// Replace this implementation with code to handle the error appropriately.
+					// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+					let nserror = error as NSError
+					fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+			  }
+		 }
+	}
 }
 
