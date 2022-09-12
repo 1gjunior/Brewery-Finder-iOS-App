9bf8e7ad65bab9a59f1c0b6001b1b252f6d08af6
Gilberto Junior <gjunior@ciandt.com>
2022-09-12 20:09:11 -0300

    feat: add favorite breweries view model

---
+++ CI&T Brewery/CI&T Brewery/FavoriteBreweries/ViewModel/FavoriteBreweriesViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteBreweriesViewModel.swift
+//  CI&T Brewery
+//
+//  Created by Gilberto Junior on 12/09/22.
+//
+
+import Combine
+import Foundation
+
+enum FavoriteBreweriesViewModelState {
+    case initial
+    case empty
+}
+
+class FavoriteBreweriesViewModel {
+    @Published private(set) var state: FavoriteBreweriesViewModelState = .initial
+}
