85d96823e4b23601a9df0530fd2fbfc0f68e21b3
Ramon Queiroz dos Santos <ramons@ciandt.com>
2022-09-13 10:47:38 -0300

    atualizando comentarios

--- CI&T Brewery/CI&T Brewery/AppDelegate.swift
+++ CI&T Brewery/CI&T Brewery/AppDelegate.swift
@@ -34,8 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		 let container = NSPersistentContainer(name: "CITModel")
 		 container.loadPersistentStores(completionHandler: { (storeDescription, error) in
 			  if let error = error as NSError? {
-				  // Replace this implementation with code to handle the error appropriately.
-				  // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 					fatalError("Unresolved error \(error), \(error.userInfo)")
 			  }
 		 })
@@ -50,8 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 			  do {
 					try context.save()
 			  } catch {
-					// Replace this implementation with code to handle the error appropriately.
-					// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 					let nserror = error as NSError
 					fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
 			  }
