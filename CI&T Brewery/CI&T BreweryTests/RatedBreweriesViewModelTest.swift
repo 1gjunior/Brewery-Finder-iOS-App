9dfff777e22c621904d1e0fdcf77a3ce90319ba7
Pedro Henrique Catanduba de Andrade <phcatanduba@ciandt.com>
2022-09-16 14:06:39 -0300

    ajuste no mock

--- CI&T Brewery/CI&T BreweryTests/RatedBreweriesViewModelTest.swift
+++ CI&T Brewery/CI&T BreweryTests/RatedBreweriesViewModelTest.swift
@@ -86,17 +86,17 @@ class BreweryRepositoryMock: BreweryRepositoryProtocol {
     }
     
     var breweries = [
-                      Brewery(id: "aaaaa", name: "test", type: "test", street: "test",
+                      Brewery(id: "aaaaa", name: "aaaaa", type: "test", street: "test",
                       address2: "test", address3: "test", city: "test", state: "test",
                       postalCode: "test", country: "test", longitude: 0.0, latitude: 0.0,
                       website: "test", phone: "test", average: 1.0, sizeEvaluations: 0.0, photos: nil),
         
-                      Brewery(id: "fopsdk", name: "test", type: "test", street: "test",
+                      Brewery(id: "fopsdk", name: "fopsdk", type: "test", street: "test",
                       address2: "test", address3: "test", city: "test", state: "test",
                       postalCode: "test", country: "test", longitude: 0.0, latitude: 0.0,
                       website: "test", phone: "test", average: 1.0, sizeEvaluations: 0.0, photos: nil),
                       
-                      Brewery(id: "ckasmplp", name: "test", type: "test", street: "test",
+                      Brewery(id: "ckasmplp", name: "ckasmplp", type: "test", street: "test",
                       address2: "test", address3: "test", city: "test", state: "test",
                       postalCode: "test", country: "test", longitude: 0.0, latitude: 0.0,
                       website: "test", phone: "test", average: 0.0, sizeEvaluations: 0.0, photos: nil)
