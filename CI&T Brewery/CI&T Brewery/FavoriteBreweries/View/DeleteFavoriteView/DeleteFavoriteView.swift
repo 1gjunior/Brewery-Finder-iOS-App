86e16a1cf206e08c58e019aeb5f27aa2d3e59802
Ramon Queiroz dos Santos <ramons@ciandt.com>
2022-09-16 14:12:37 -0300

    ajustes na tela

--- CI&T Brewery/CI&T Brewery/FavoriteBreweries/View/DeleteFavoriteView/DeleteFavoriteView.swift
+++ CI&T Brewery/CI&T Brewery/FavoriteBreweries/View/DeleteFavoriteView/DeleteFavoriteView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DeleteFavoriteView: UIView{
 	
+	@IBOutlet var mainView: UIView!
 	@IBOutlet weak var modalView: UIView!{
 		didSet{
 			modalView.layer.cornerRadius = 20
@@ -18,24 +19,54 @@ class DeleteFavoriteView: UIView{
 	@IBOutlet weak var lbTitle: UILabel!{
 		didSet{
 			lbTitle.text = NSLocalizedString("deleteFavoriteTitle", comment: "")
-			lbTitle.font = UIFont.robotoRegular(ofSize: 16)
+			lbTitle.font = UIFont.robotoRegular(ofSize: 18)
 			lbTitle.textColor = UIColor.breweryBlack()
 		}
 	}
 	@IBOutlet weak var lbText: UILabel!{
 		didSet{
 			lbText.text = NSLocalizedString("deleteFavoriteText", comment: "")
-			lbTitle.font = UIFont.robotoLight(ofSize: 12)
-			lbTitle.textColor = UIColor.breweryBlack()
+			lbText.font = UIFont.robotoRegular(ofSize: 16)
+			lbText.textColor = UIColor.breweryBlack()
 		}
 	}
-	@IBOutlet weak var cancelButton: UIButton!
-	@IBOutlet weak var confirmButton: UIButton!
+	@IBOutlet weak var cancelButton: UIButton!{
+		didSet{
+			cancelButton?.layer.borderColor = UIColor.breweryBlack().cgColor
+			cancelButton?.layer.borderWidth = 1
+			cancelButton?.layer.cornerRadius = 18
+		}
+	}
+	@IBOutlet weak var confirmButton: UIButton!{
+		didSet {
+			confirmButton?.layer.borderColor = UIColor.breweryYellowLight().cgColor
+			confirmButton?.layer.borderWidth = 1
+			confirmButton?.layer.cornerRadius = 18
+			confirmButton?.layer.backgroundColor = UIColor.breweryYellowLight().cgColor
+		}
+	}
+
+	override init(frame: CGRect) {
+		 super.init(frame: frame)
+		 commonInit()
+	}
 	
+	required init?(coder: NSCoder) {
+		 super.init(coder: coder)
+		 commonInit()
+	}
+	
+	public func commonInit() {
+		 guard let viewFromXib = Bundle.main.loadNibNamed("DeleteFavoriteView", owner: self, options: nil)?[0] as? UIView else { return }
+		 viewFromXib.frame = self.bounds
+		 addSubview(viewFromXib)
+	}
 	
 	@IBAction func cancelRemoval(_ sender: UIButton) {
+		print("cancelou")
 	}
 	@IBAction func confirmRemoval(_ sender: UIButton) {
+		print("excluiu")
 	}
-	
+
 }
