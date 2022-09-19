9de273fe70d394d863ff5cf9607f3bbb5b34ce17
Rafaela Souza <rafaelasouza@ciandt.com>
2022-09-19 17:29:24 -0300

    Testes dofetchTop10 da HomeViewModel e refatoracao

---
+++ CI&T Brewery/CI&T BreweryTests/Mock/BreweryMock.swift
@@ -0,0 +1,8 @@
+//
+//  BreweryMock.swift
+//  CI&T BreweryTests
+//
+//  Created by Rafaela Cristina Souza dos Santos on 19/09/22.
+//
+
+import Foundation
---
+++ CI&T Brewery/CI&T BreweryTests/Mock/FavoriteBreweriesUseCaseMock.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteBreweriesUseCaseMock.swift
+//  CI&T BreweryTests
+//
+//  Created by Rafaela Cristina Souza dos Santos on 19/09/22.
+//
+
+import Foundation
