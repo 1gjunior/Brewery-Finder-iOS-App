eb6306eed913bbe10360a653d15651ef5cf5e948
Rafaela Souza <rafaelasouza@ciandt.com>
2022-09-21 14:04:57 -0300

    Implementando testes do repository com os metodos da BreweryDetailViewModel

--- CI&T Brewery/CI&T BreweryTests/BreweryRepositoryTest.swift
+++ CI&T Brewery/CI&T BreweryTests/BreweryRepositoryTest.swift
@@ -13,6 +13,7 @@ class BreweryRepositoryTest: XCTestCase {
     let apiManagerMock = APIManagerMock()
     let breweries = BreweryMock.breweries
     let brewery = BreweryMock.breweries[0]
+    let photos = BreweryMock.breweryPhotos
     let error = NSError(domain: "", code: 401, userInfo: [ NSLocalizedDescriptionKey: "Error"])
     
     override func setUp() {
@@ -81,6 +82,70 @@ class BreweryRepositoryTest: XCTestCase {
             switch result {
             case .success:
                 XCTFail("Error test_getBreweryByIdError BreweryRepositoryTest")
+            case .failure(let error):
+                XCTAssertNotNil(error)
+            }
+        }
+    }
+    
+    func test_getRatedBreweriesSuccess() {
+        // given
+        apiManagerMock.item = breweries
+        
+        // when
+        repository.getRatedBreweries(email: "teste@teste.com") { result in
+            // then
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure:
+                XCTFail("Error test_getRatedBreweriesSuccess BreweryRepositoryTest")
+            }
+        }
+    }
+    
+    func test_getRatedBreweriesError() {
+        // given
+        apiManagerMock.error = error
+        
+        // when
+        repository.getRatedBreweries(email: "teste@teste.com") { result in
+            // then
+            switch result {
+            case .success:
+                XCTFail("Error test_getRatedBreweriesError BreweryRepositoryTest")
+            case .failure(let error):
+                XCTAssertNotNil(error)
+            }
+        }
+    }
+    
+    func test_getBreweryPhotosSuccess() {
+        // given
+        apiManagerMock.item = photos
+        
+        // when
+        repository.getBreweryPhotos(id: "alphabet-city-brewing-co-new-york") { result in
+            // then
+            switch result {
+            case .success(let data):
+                XCTAssertNotNil(data)
+            case .failure:
+                XCTFail("Error test_getBreweryPhotosSuccess BreweryRepositoryTest")
+            }
+        }
+    }
+    
+    func test_getBreweryPhotosError() {
+        // given
+        apiManagerMock.error = error
+        
+        // when
+        repository.getBreweryPhotos(id: "alphabet-city-brewing-co-new-york") { result in
+            // then
+            switch result {
+            case .success:
+                XCTFail("Error test_getBreweryPhotosError BreweryRepositoryTest")
             case .failure(let error):
                 XCTAssertNotNil(error)
             }
